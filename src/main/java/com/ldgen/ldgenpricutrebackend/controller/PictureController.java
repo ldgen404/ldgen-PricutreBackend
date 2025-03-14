5a316f84486a9416a3e253765242412ef0869e70
lbiggen <1538574120@qq.com>
2025-03-14 14:25:17 +0800

    图片优化-使用缓存1、redis缓存+2、Caffeine缓存

--- src/main/java/com/ldgen/ldgenpricutrebackend/controller/PictureController.java
+++ src/main/java/com/ldgen/ldgenpricutrebackend/controller/PictureController.java
@@ -1,7 +1,11 @@
 package com.ldgen.ldgenpricutrebackend.controller;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.ldgen.ldgenpricutrebackend.annotation.AuthCheck;
 import com.ldgen.ldgenpricutrebackend.common.BaseResponse;
 import com.ldgen.ldgenpricutrebackend.common.DeleteRequest;
@@ -20,14 +24,19 @@ import com.ldgen.ldgenpricutrebackend.service.PictureService;
 import com.ldgen.ldgenpricutrebackend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -45,6 +54,19 @@ public class PictureController {
     @Resource
     private PictureService pictureService;
 
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 本地缓存
+     */
+    private final Cache<String, String> LOCAL_CACHE = Caffeine.newBuilder()
+            .initialCapacity(1024)
+            .maximumSize(10_000)
+//            缓存五分钟后移除
+            .expireAfterWrite(Duration.ofMinutes(5))
+            .build();
 
     /**
      * 上传图片（可重新上传）
@@ -180,10 +202,60 @@ public class PictureController {
         // 查询数据库
         Page<Picture> picturePage = pictureService.page(new Page<>(current, size), pictureService.getQueryWrapper(pictureQueryRequest));
 
-
         // 获取封装类
         return ResultUtils.success(pictureService.getPictureVOPage(picturePage, request));
     }
+
+    @PostMapping("/list/page/vo/cache")
+    public BaseResponse<Page<PictureVO>> listPictureVOByPageWithCache(@RequestBody PictureQueryRequest pictureQueryRequest,
+                                                                      HttpServletRequest request) {
+        long current = pictureQueryRequest.getCurrent();
+        long size = pictureQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        // 普通用户默认只能查看已过审的数据
+        pictureQueryRequest.setReviewStatus(PictureReviewStatusEnum.PASS.getValue());
+
+
+        // 构建缓存 key
+        String queryCondition = JSONUtil.toJsonStr(pictureQueryRequest);
+        String hashKey = DigestUtils.md5DigestAsHex(queryCondition.getBytes());
+//        String redisKey = "Ldgencture:listPictureVOByPage:" + hashKey;
+        // 从 Redis 缓存中查询
+        // 构建缓存 key
+        String cacheKey = "listPictureVOByPage:" + hashKey;
+// 从本地缓存中查询
+        String cachedValue = LOCAL_CACHE.getIfPresent(cacheKey);
+//        String cachedValue = valueOps.get(redisKey);
+        ValueOperations<String, String> valueOps = stringRedisTemplate.opsForValue();
+        if (cachedValue != null) {
+            // 如果缓存命中，返回结果
+            Page<PictureVO> cachedPage = JSONUtil.toBean(cachedValue, Page.class);
+            return ResultUtils.success(cachedPage);
+        }
+
+        // 查询数据库
+        Page<Picture> picturePage = pictureService.page(new Page<>(current, size),
+                pictureService.getQueryWrapper(pictureQueryRequest));
+        QueryWrapper<Picture> queryWrapper = pictureService.getQueryWrapper(pictureQueryRequest);
+        System.out.println("生成的 SQL：" + queryWrapper.getSqlSegment());
+        System.out.println("查询条件：" + pictureQueryRequest);
+        // 获取封装类
+        Page<PictureVO> pictureVOPage = pictureService.getPictureVOPage(picturePage, request);
+
+// 存入本地缓存
+        String cacheValue = JSONUtil.toJsonStr(pictureVOPage);
+        // 存入 Redis 缓存
+//        String cacheValue = JSONUtil.toJsonStr(pictureVOPage);
+        // 5 - 10 分钟随机过期，防止雪崩
+//        int cacheExpireTime = 300 + RandomUtil.randomInt(0, 300);
+//        valueOps.set(redisKey, cacheValue, cacheExpireTime, TimeUnit.SECONDS);
+        LOCAL_CACHE.put(cacheKey, cacheValue);
+
+        // 返回结果
+        return ResultUtils.success(pictureVOPage);
+    }
+
 
     /**
      * 编辑图片（给用户使用）
--- src/test/java/com/ldgen/ldgenpricutrebackend/LgenPricutreBackendApplicationTests.java
+++ src/test/java/com/ldgen/ldgenpricutrebackend/LgenPricutreBackendApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ldgen.ldgenpricutrebackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class LgenPricutreBackendApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     void contextLoads() {
+
     }
 
 }
