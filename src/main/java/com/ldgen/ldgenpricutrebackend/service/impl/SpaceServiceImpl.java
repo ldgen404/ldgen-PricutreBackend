7c8874ba67094534209850a1187b87c1cef7658c
lbiggen <1538574120@qq.com>
2025-03-15 23:00:15 +0800

    第二期空间模块初始化开始

---
+++ src/main/java/com/ldgen/ldgenpricutrebackend/service/impl/SpaceServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ldgen.ldgenpricutrebackend.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ldgen.ldgenpricutrebackend.model.entity.Space;
+import com.ldgen.ldgenpricutrebackend.mapper.SpaceMapper;
+import com.ldgen.ldgenpricutrebackend.service.SpaceService;
+import org.springframework.stereotype.Service;
+
+/**
+* @author 15385
+* @description 针对表【space(空间)】的数据库操作Service实现
+* @createDate 2025-03-15 22:52:53
+*/
+@Service
+public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
+    implements SpaceService {
+
+}
+
+
+
+
